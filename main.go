c3d2b6804bde00aa76376b7eec2982886c4d69cd
Wenxin Liu <wenxin.liu@springer.com>
2020-02-13 10:36:43 +0000

    Instrumented three mock metrics for Prometheus
    Instrumented three mock metrics (boring_count, special_count, response_count). boring_count increments by 1 every time any path apart from /special and /metrics is requested. special_count increments by 1 every time /special is requested. request_count increments by 1 every time any path apart from /metrics is requested.
    
    Metrics are exposed via the /metrics endpoint.

---
+++ main.go
@@ -0,0 +1,54 @@
+package main
+
+import (
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/promauto"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
+	"log"
+	"net/http"
+)
+
+var (
+	BoringCounter = promauto.NewCounter(prometheus.CounterOpts{
+		Name: "boring_count",
+	})
+	SpecialCounter = promauto.NewCounter(prometheus.CounterOpts{
+		Name: "special_count",
+	})
+	RequestCounter = promauto.NewCounter(prometheus.CounterOpts{
+		Name: "request_count",
+	})
+)
+
+type MyDefaultHandler struct {
+
+}
+
+type SpecialHandler struct {
+
+}
+
+func (m MyDefaultHandler) ServeHTTP(w http.ResponseWriter, r *http.Request){
+	log.Printf("Serving request for path %s", r.URL.Path)
+	w.Write([]byte(r.URL.Path))
+	BoringCounter.Inc()
+	RequestCounter.Inc()
+
+}
+
+func (s SpecialHandler) ServeHTTP(w http.ResponseWriter, r *http.Request){
+	log.Println("Special handler is called")
+	w.Write([]byte("Special"))
+	SpecialCounter.Inc()
+	RequestCounter.Inc()
+}
+
+
+func main() {
+	mux := http.NewServeMux()
+	mux.Handle("/special", SpecialHandler{})
+	mux.Handle("/", MyDefaultHandler{})
+	mux.Handle("/metrics", promhttp.Handler())
+	server := http.Server{Addr: "127.0.0.1:8080", Handler: mux}
+	server.ListenAndServe()
+}
